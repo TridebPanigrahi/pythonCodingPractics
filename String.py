41ba17de2b9c8ec75c997408a6270698eabfa651
trideb-religh <panigrahitrideb1@gmail.com>
2023-07-15 16:34:06 +0530

    updated

---
+++ String.py
@@ -0,0 +1,10 @@
+# # Write a python program to check a string is palindrone or not
+
+# s = str(input("Enter a word: "))
+# def isPalindrome(s):
+#     return s ==s[::-1]
+
+# print(isPalindrome(s))
+
+# ///////////////////////////////////////////////////////////////
+
