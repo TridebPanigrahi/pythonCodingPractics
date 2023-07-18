6f39df399ce8b5b862b43dc3ac7a48ae46276624
trideb-religh <panigrahitrideb1@gmail.com>
2023-07-18 11:07:34 +0530

    updated

--- String.py
+++ String.py
@@ -8,3 +8,18 @@
 
 # ///////////////////////////////////////////////////////////////
 
+# # Write a python program to print even position charecter in a string
+
+# s= str(input("Enter a word: "))
+# def findEvenPosition(str):
+#     for i in range(len(str)):
+#         if(i%2==0):
+#             print(str[i])
+
+# findEvenPosition(s)
+
+# ///////////////////////////////////////////////////////////////
+
+# Write a python program to remove duplicate charecter of a string
+
+
